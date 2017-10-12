91cc45a8de71c8b0970f34942e553fea68bb4f12
Rogu_ss76 <Rogu_ss76@305-7P.od.itstep.local>
2017-10-12 20:54:31 +0300

    next 2

--- smplApp/src/smplApp/Program.java
+++ smplApp/src/smplApp/Program.java
@@ -1,18 +1,34 @@
 package smplApp;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import smplApp.utils.dao.DeviceDAO;
+import smplApp.utils.dao.DeviceDAOImpl;
+
 public class Program {
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-		Network net1 = (Network) ctx.getBean("network");
-		net1.getDev().setId(144);
-		net1.getDev().setName("My Network device");
-		net1.getDev().setLocation(net1.getName());
-		System.out.println(net1);
-		System.out.println(net1.getDev());
+	public static void main(String[] args) throws ClassNotFoundException {
+//		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+//		Network net1 = (Network) ctx.getBean("network");
+//		net1.getDev().setId(144);
+//		net1.getDev().setName("My Network device");
+//		net1.getDev().setLocation(net1.getName());
+//		System.out.println(net1);
+//		System.out.println(net1.getDev());
+		
+		ApplicationContext gtx = new ClassPathXmlApplicationContext("datasource.xml");
+		DeviceDAOImpl devDAO = (DeviceDAOImpl) gtx.getBean("deviceDao");
+		System.out.println(devDAO.toString());
+		
+		List<Device> devs;
+		devs = devDAO.getAll();
+		System.out.println("ALL DEVICES:\n");
+		for(Device d: devs) {
+			System.out.println(d);
+		}
 	}
 
 }
--- smplApp/src/smplApp/utils/dao/DeviceDAOImpl.java
+++ smplApp/src/smplApp/utils/dao/DeviceDAOImpl.java
@@ -2,17 +2,22 @@ package smplApp.utils.dao;
 
 import java.util.List;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import smplApp.Device;
 
 public class DeviceDAOImpl implements DeviceDAO {
 	JdbcTemplate jdbcTemplate;
+	DataSource dataSource;
 	
+	public DeviceDAOImpl(DataSource ds) {
+		this.dataSource = ds;
+		this.jdbcTemplate = new JdbcTemplate(this.dataSource);
+	}
 	
 	@Override
 	public List<Device> getAll() {
-		return null;
+		return this.jdbcTemplate.query(GET_ALL, new smplApp.utils.dao.mappers.DeviceMapper());
 	}
-
 }
